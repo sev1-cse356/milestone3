f5c2245e45ad542e052bcdba4bf2e08ddff4aac0
Jie Chen <jiechenmc@gmail.com>
2024-10-01 09:23:17 -0400

    update mailer

--- src/mailer.js
+++ src/mailer.js
@@ -14,18 +14,18 @@ const transporter = nodemailer.createTransport({
 });
 
 // async..await is not allowed in global scope, must use a wrapper
-async function main() {
+module.exports.sendVerificationEmail = async function sendVerificationEmail({
+  to,
+  text,
+}) {
   // send mail with defined transport object
   const info = await transporter.sendMail({
     from: '"TEST" <root@sev-1.cse356.compas.cs.stonybrook.edu>', // sender address
-    to: "jiechenmc2@gmail.com", // list of receivers
-    subject: "Hello ✔", // Subject line
-    text: "Hello world?", // plain text body
-    html: "<b>Hello world?</b>", // html body
+    to, // list of receivers
+    subject: "Your Verification Code", // Subject line
+    text, // plain text body
   });
 
   console.log("Message sent: %s", info.messageId);
   // Message sent: <d786aa62-4e0a-070a-47ed-0b0666549519@ethereal.email>
-}
-
-main().catch(console.error);
+};
--- src/server.js
+++ src/server.js
@@ -1,5 +1,6 @@
 const express = require("express");
 const session = require("express-session");
+const { sendVerificationEmail } = require("./mailer");
 
 const app = express();
 const port = 3000;
@@ -20,19 +21,27 @@ app.use("", (req, res, next) => {
   next();
 });
 
-app.post("/adduser", (req, res) => {
+app.post("/adduser", async (req, res) => {
   const { username, password, email } = req.body;
   db[email] = { username, password, email, disabled: true };
-  return res.json({ username, password, email, disabled: true });
+  await sendVerificationEmail(
+    email,
+    `${req.headers.host}/verify?email=${email}&key="somerandomstring"`
+  );
+  return res.send("OK");
 });
 
 app.get("/verify", (req, res) => {
   const { email, key } = req.query;
   if (key) {
     db[email].disabled = false;
-    return res.send(`${email} Verified`);
+    return res.send(`OK`);
   }
-  return res.send(`${email} Not Verified`);
+  return res.send({
+    status: "ERROR",
+    error: true,
+    message: "your error message",
+  });
 });
 
 app.post("/login", (req, res) => {
@@ -41,16 +50,25 @@ app.post("/login", (req, res) => {
     if (e.username === username && e.password === password && !e.disabled) {
       req.session.username = username;
       req.session.email = email;
-      return res.send(req.session);
+      return res.send("OK");
     }
   });
-  return res.send("Fail to Login");
+  return res.send({
+    status: "ERROR",
+    error: true,
+    message: "your error message",
+  });
 });
 
 app.post("/logout", (req, res) => {
   req.session.destroy(function (err) {
-    if (err) res.send("User Did Not Log Out");
-    else res.send("User Logged Out");
+    if (err)
+      res.send({
+        status: "ERROR",
+        error: true,
+        message: "your error message",
+      });
+    else res.send("OK");
   });
 });
 
