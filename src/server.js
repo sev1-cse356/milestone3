9d7d2b554a0b4a1074eceee313aa8dd405fc22d8
Jie Chen <jiechenmc@gmail.com>
2024-10-02 23:41:01 -0400

    more debug

--- src/server.js
+++ src/server.js
@@ -83,9 +83,11 @@ app.post("/login", (req, res) => {
       !entry.disabled
     ) {
       req.session.username = username;
+      console.log("MATCH");
       return res.json({ status: "OK" });
     }
   });
+  console.log("NO MATCH");
   return res.json({
     status: "ERROR",
     error: true,
