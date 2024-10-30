8fd3479fd5a6235e245d57709f224ee644c82953
Jie Chen <jiechenmc@gmail.com>
2024-10-30 16:34:13 -0400

    remove original file

--- scripts/resize.sh
+++ scripts/resize.sh
@@ -11,7 +11,7 @@ for video in *.mp4; do
   ffmpeg -i "$video" \
   -vf "scale=w=iw*min(1280/iw\,720/ih):h=ih*min(1280/iw\,720/ih),pad=1280:720:(1280-iw*min(1280/iw\,720/ih))/2:(720-ih*min(1280/iw\,720/ih))/2" \
   -c:a copy \
-  "${filename}_padded.mp4"
+  "${filename}.mp4"
 done
 
 cd $home
