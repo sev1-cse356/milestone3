4f300df61e8f7e2484dbdbf05b4c10df0a08a400
Jie Chen <jiechenmc@gmail.com>
2024-11-10 12:11:04 -0500

    debug in prod

--- src/routes/milestone1.js
+++ src/routes/milestone1.js
@@ -14,6 +14,7 @@ MileStone1Router.post("/adduser", async (req, res) => {
       message: "DUPLICATE",
     });
   db[email] = { username, password, email, disabled: true, viewed: new Set() };
+  console(email);
   if (email !== "admin@356.com") {
     await sendVerificationEmail(
       email,
