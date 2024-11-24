61331e8e7ffdda31bf05f0900e684da99404881d
Jie Chen <jiechenmc@gmail.com>
2024-11-24 14:42:05 -0500

    skibidi bro :(

--- script.js
+++ script.js
@@ -2,30 +2,47 @@ import http from "k6/http";
 
 export const options = {
   // A number specifying the number of VUs to run concurrently.
-  vus: 10,
+  vus: 1,
   // A string specifying the total duration of the test run.
   duration: "30s",
 };
 
-const binFile = open("ms3.mp4", "b");
+const admin = {
+  username: "admin",
+  password: "abc123",
+  email: "admin@356.com",
+ 
+}
 
-// The function that defines VU logic.
-//
-// See https://grafana.com/docs/k6/latest/examples/get-started-with-k6/ to learn more
-// about authoring k6 scripts.
-//
+const baseUrl = "http://localhost"
+// const binFile = open("ms3.mp4", "b");
+
+export function setup() {
+
+  const secret =  "somerandomstring"
+  
+  http.post(`${baseUrl}/api/adduser`, admin);
+
+  http.post(`${baseUrl}/api/verify?email=${admin.email}&key=${secret}`);
+
+  http.post(`${baseUrl}/api/login`, {
+    "username": admin.username,
+    "password": admin.password
+  });
+
+  console.log(http.cookieJar())
+
+}
+
 export default function () {
-  http.post("http://localhost/api/like", {
-    id: "test",
+
+  http.post(`${baseUrl}/api/like`, {
+    id: "1732336605494",
     value: true,
   });
 
-  // const data = {
-  //   author: 'this is a standard form field',
-  //   title: "TEST",
-  //   description: "YEET",
-  //   mp4File: http.file(binFile, 'ms3.mp4'),
-  // };
+}
 
-  // http.post('http://localhost/api/upload', data);
+export function teardown(data) {
+  // 4. teardown code
 }
